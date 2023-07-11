fc88cfb91d58bc2d6a2738afd8446785fb119d43
dingdingding60 <36698195+dingdingding60@users.noreply.github.com>
2023-07-11 18:45:48 +0200

    Delete run5.sh

--- sharpness/run5.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python sharpness.py --model ResNet10 \
---model_file /root/autodl-tmp/checkpoints/miniImagenet/ResNet10_maml_approx_5way_1shot_seed5/best_model.tar --split base
